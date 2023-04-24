5ad067b0e0d5c23ffe04b56de2f17d6751587109
Samuel-045 <samuelvic856@gmail.com>
2023-04-24 19:17:30 -0300

    Realocação das imagens e organização dos preços

--- InterfaceGráfica2.java
+++ InterfaceGráfica2.java
@@ -21,44 +21,44 @@ import java.awt.event.*;
 		
 		private void inicializarComponentes() {
 			
-			bemvindo = new JLabel("Bem vindo ao Sacolão zé Rodolfo. Faça suas compras");
+			bemvindo = new JLabel("Bem vindo ao Hortifrúti zé Rodolfo. Faça suas compras");
 			bemvindo.setForeground(Color.blue);
 			bemvindo.setBounds(320, 0, 500, 50);
 			add(bemvindo);
 			
 			iabacaxi = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\abacaxi1.png");
 			Jabacaxi = new JLabel(iabacaxi);
-			Jabacaxi.setBounds(0, 300, 180, 300);
+			Jabacaxi.setBounds(0, 320, 170, 300);
 			add(Jabacaxi);
 		    Jabacaxi.setVisible(false);
 			
 			ibanana = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\banana.jpg");
 			Jbanana = new JLabel(ibanana);
-			Jbanana.setBounds(150 ,300 , 180, 300);
+			Jbanana.setBounds(150 ,320 , 180, 300);
 			add(Jbanana);
 			Jbanana.setVisible(false);
 			
 			imaça = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\maça.jpg");
 			Jmaça = new JLabel(imaça);
-			Jmaça.setBounds(320, 300, 180, 300);
+			Jmaça.setBounds(325, 320, 180, 300);
 			add(Jmaça);
 			Jmaça.setVisible(false);
 				
 			ipera = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\pera.jpg");
 			Jpera= new JLabel(ipera);
-			Jpera.setBounds(480, 300, 180, 300);
+			Jpera.setBounds(507, 320, 180, 300);
 			add(Jpera);
 			Jpera.setVisible(false);
 			
 			iuva = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\uva1.jpg");
 			Juva= new JLabel(iuva);
-			Juva.setBounds(660, 300, 180, 300);
+			Juva.setBounds(687,320, 180, 300);
 			add(Juva);
 			Juva.setVisible(false);
 			
 			imelancia = new ImageIcon("C:\\Users\\Ana Paula\\eclipse-workspace\\terceirosemestre\\src\\sacolao\\imgs\\melancia.jpg");
 			Jmelancia= new JLabel(imelancia);
-			Jmelancia.setBounds(840, 300, 180, 300);
+			Jmelancia.setBounds(870, 320, 180, 300);
 			add(Jmelancia);
 			Jmelancia.setVisible(false);
 			
@@ -86,33 +86,33 @@ import java.awt.event.*;
 			
 			//frutas (produtos)
 			//banana
-			ckbanana = new  JCheckBox("Banana");
-			ckbanana.setBounds(400,125,100,25);
+			ckbanana = new  JCheckBox("Banana R$7,00");
+			ckbanana.setBounds(400,125,150,25);
 			add(ckbanana);
 			
 			//maça
-			maca = new  JCheckBox("Maça");
-			maca.setBounds(700,125,100,25);
+			maca = new  JCheckBox("Maça R$6,00");
+			maca.setBounds(700,125,150,25);
 			add(maca);
 			
 			//banana
-			uva = new  JCheckBox("Uva");
-			uva.setBounds(70,125,100,25);
+			uva = new  JCheckBox("Uva R$10,00");
+			uva.setBounds(70,125,150,25);
 			add(uva);
 			
 			//pera
-			pera = new  JCheckBox("Pera");
-			pera.setBounds(400,75,100,25);
+			pera = new  JCheckBox("Pera R$13,00");
+			pera.setBounds(400,75,150,25);
 			add(pera);
 			
 			//abacaxi
-			abacaxi = new  JCheckBox("Abacaxi");
-			abacaxi.setBounds(700,75,100,25);
+			abacaxi = new  JCheckBox("Abacaxi R$10,00");
+			abacaxi.setBounds(700,75,150,25);
 			add(abacaxi);
 			
 			//melancia
-			melancia = new  JCheckBox("Melancia");
-			melancia.setBounds(70,75,100,25);
+			melancia = new  JCheckBox("Melancia R$20,00");
+			melancia.setBounds(70,75,150,25);
 			add(melancia);
 			
 			//mensagem
@@ -176,10 +176,10 @@ import java.awt.event.*;
 		}
 				
 	public static void main(String args[]){
-		 JFrame frame = new JFrame("Sacolão do zé Rodolfo");
+		 JFrame frame = new JFrame("Hortifrúti do zé Rodolfo");
 		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 frame.getContentPane().add(new InterfaceGráfica2());
-		 frame.setBounds(100,10,1040,670);
+		 frame.setBounds(100,5,1080,670);
 		 frame.validate();
 		 frame.setVisible(true);
 	}
